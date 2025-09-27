a80dcfc93ed92cb37e48ca85ca2a5118aa317055
Ankityadavhere <omkar6784@gmail.com>
2025-09-27 23:24:58 +0530

    Update app.js
    Add GalaxyBackground class with canvas, stars, nebulae, mouse tracking, and animation

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,1 +1,22 @@
 
+class GalaxyBackground {
+    constructor() {
+        this.canvas = document.getElementById('galaxy-canvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.stars = [];
+        this.shootingStars = [];
+        this.nebulae = [];
+        this.mouse = { x: 0, y: 0 };
+        this.isMouseMoving = false;
+        this.mouseTimeout = null;
+        
+        this.init();
+    }
+    
+    init() {
+        this.resizeCanvas();
+        this.createStars();
+        this.bindEvents();
+        this.animate();
+    }
+    
